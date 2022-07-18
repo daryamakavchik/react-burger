a3ba659f6a1600ee4f98dc29578012d3d7f8466b
daryamakavchik <darjamakavchik@gmail.com>
2022-07-17 20:43:47 -0700

    Updates

---
+++ precache-manifest.688c784ee9c5be83790940d29ce97acd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "abb4a1c899b1dec405e2",
+    "url": "./static/css/main.fe584b46.chunk.css"
+  },
+  {
+    "revision": "abb4a1c899b1dec405e2",
+    "url": "./static/js/main.abb4a1c8.chunk.js"
+  },
+  {
+    "revision": "aa28d4936cf04d7d3971",
+    "url": "./static/css/1.1cdba2ab.chunk.css"
+  },
+  {
+    "revision": "aa28d4936cf04d7d3971",
+    "url": "./static/js/1.aa28d493.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4ec5acf8515210125e80cf53d4a49ea3",
+    "url": "./static/media/JetBrainsMono-Bold.4ec5acf8.woff2"
+  },
+  {
+    "revision": "2b64112a72baa46a570c02f6a4b4f2c7",
+    "url": "./static/media/JetBrainsMono-Regular.2b64112a.woff"
+  },
+  {
+    "revision": "13359a07227ea33ac84fc520f4dd172c",
+    "url": "./static/media/JetBrainsMono-Regular.13359a07.woff2"
+  },
+  {
+    "revision": "974d6fa62992dad387572d8f3a13e8cc",
+    "url": "./static/media/JetBrainsMono-Bold.974d6fa6.woff"
+  },
+  {
+    "revision": "190e82e91f8b44f23b28d50584aca336",
+    "url": "./static/media/Iceland-Regular.190e82e9.woff2"
+  },
+  {
+    "revision": "b2cd58348e7bc3ea2f8035d824c80396",
+    "url": "./static/media/Iceland-Regular.b2cd5834.woff"
+  },
+  {
+    "revision": "0c0abc2883d68596a6ae849a1b2d74ac",
+    "url": "./static/media/graphics.0c0abc28.svg"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "./static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "./static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "./static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "./static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "./static/media/iceland.df042be2.woff"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "./static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "d7a6031c599ccafeea70d5113cc0e597",
+    "url": "./index.html"
+  }
+];
