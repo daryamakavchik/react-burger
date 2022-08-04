9f3ab61d766d2104851e3eae58086a344a607a7d
daryamakavchik <darjamakavchik@gmail.com>
2022-08-03 18:00:47 -0700

    feat: feed and orderinfo page

---
+++ src/components/ordercard/ordercard.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styles from '../../pages/feed.module.css';
+import preview from "../../images/ingredientpreview.png";
+import preview2 from "../../images/ingredientpreview2.png";
+import preview3 from "../../images/ingredientpreview3.png";
+import preview4 from "../../images/ingredientpreview4.png";
+import preview5 from "../../images/ingredientpreview5.png";
+import { useRouteMatch } from "react-router-dom";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { selectOrderAction } from "../../services/actions/feed";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useLocation } from 'react-router-dom';
+
+export default function OrderCard({order}){
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { url } = useRouteMatch();
+  const { number, ingredients, createdAt, name, _id, status } = order;
+  const date = new Date();
+  const cost = React.useMemo(
+    () => ingredients.reduce((acc, cur) => acc + cur.price * cur.quantity, 0),
+    [ingredients]
+  );
+
+    return (
+        <li
+        className={styles.order}
+        onClick={() => dispatch(selectOrderAction(order))}
+      >
+        <Link
+          to={{ pathname: `${url}/${_id}`, state: { orderCard: location } }}
+        >
+          <div className={styles.orderid}>
+            <p
+              className={`${styles.id} text text_type_digits-default`}
+            >{`#${number}`}</p>
+            <p
+              className={`${styles.timestamp} text text_type_main-small text_color_inactive`}
+            >
+              {date}
+            </p>
+          </div>
+          <h3
+            className={`${styles.burgername} text text_type_main-default`}
+          >
+            {name}
+          </h3>
+          {url === "/profile/order" && (
+            <p
+              className={`text text_type_main-small ${
+                status === "done" && styles.ready
+              }`}
+            >
+              {status === "created"
+                ? "Создан"
+                : status === "pending"
+                ? "Готовится"
+                : "Выполнен"}
+            </p>
+          )}
+          <div className={styles.componentandprice}>
+            <div className={styles.ingredients}>
+              <div className={styles.preview}>
+                <div className={styles.illustration}>
+                  <img src={preview} />
+                </div>
+              </div>
+              <div className={styles.preview}>
+                <div className={styles.illustration}>
+                  <img src={preview2} />
+                </div>
+              </div>
+              <div className={styles.preview}>
+                <div className={styles.illustration}>
+                  <img src={preview3} />
+                </div>
+              </div>
+              <div className={styles.preview}>
+                <div className={styles.illustration}>
+                  <img src={preview4} />
+                </div>
+              </div>
+              <div className={styles.preview}>
+                <div className={styles.illustration}>
+                  <img src={preview5} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.price}>
+              <p
+                className={`${styles.digit} text text_type_digits-default`}
+              >
+                480
+              </p>
+              <CurrencyIcon type="primary" />
+            </div>
+          </div>
+        </Link>
+      </li>
+    )
+}
--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,147 +1,156 @@
-import React from 'react';
-import styles from './feed.module.css';
-import preview from '../images/ingredientpreview.png';
-import preview2 from '../images/ingredientpreview2.png';
-import preview3 from '../images/ingredientpreview3.png';
-import preview4 from '../images/ingredientpreview4.png';
-import preview5 from '../images/ingredientpreview5.png';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import React from "react";
+import styles from "./feed.module.css";
+import { useDispatch } from "react-redux";
+import OrderCard from "../components/ordercard/ordercard";
+import { useSelector } from 'react-redux';
+import { wsConnectionClosedAction, wsConnectionStartAction } from "../services/actions/ws";
+import { setCorrectOrdersAction, setDoneOrdersAction } from "../services/actions/feed";
 
-export function FeedPage(){
-    return (
-        <>
-            <h2 className={`${styles.title} text text_type_main-large`}>Лента заказов</h2>
-            <div className={styles.content}>
-                <ul className={styles.orders}>
-                    <li className={styles.order}>
-                        <div className={styles.orderid}>
-                            <p className={`${styles.id} text text_type_digits-default`}>#034535</p>
-                            <p className={`${styles.timestamp} text text_type_main-small text_color_inactive`}>Сегодня, 16:20 i-GMT+3</p>
-                        </div>
-                        <h3 className={`${styles.burgername} text text_type_main-default`}>Death Star Starship Main бургер</h3>
-                        <div className={styles.componentandprice}>
-                            <div className={styles.ingredients}>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview2} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview3} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview4} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview5} /></div></div>
-                            </div>
-                            <div className={styles.price}>
-                                <p className={`${styles.digit} text text_type_digits-default`}>480</p>
-                                <CurrencyIcon type="primary" />
-                            </div>
-                        </div>
-                    </li>
-                    <li className={styles.order}>
-                        <div className={styles.orderid}>
-                            <p className={`${styles.id} text text_type_digits-default`}>#034535</p>
-                            <p className={`${styles.timestamp} text text_type_main-small text_color_inactive`}>Сегодня, 16:20 i-GMT+3</p>
-                        </div>
-                        <h3 className={`${styles.burgername} text text_type_main-default`}>Death Star Starship Main бургер</h3>
-                        <div className={styles.componentandprice}>
-                            <div className={styles.ingredients}>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview2} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview3} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview4} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview5} /></div></div>
-                            </div>
-                            <div className={styles.price}>
-                                <p className={`${styles.digit} text text_type_digits-default`}>480</p>
-                                <CurrencyIcon type="primary" />
-                            </div>
-                        </div>
-                    </li>
-                    <li className={styles.order}>
-                        <div className={styles.orderid}>
-                            <p className={`${styles.id} text text_type_digits-default`}>#034535</p>
-                            <p className={`${styles.timestamp} text text_type_main-small text_color_inactive`}>Сегодня, 16:20 i-GMT+3</p>
-                        </div>
-                        <h3 className={`${styles.burgername} text text_type_main-default`}>Death Star Starship Main бургер</h3>
-                        <div className={styles.componentandprice}>
-                            <div className={styles.ingredients}>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview2} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview3} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview4} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview5} /></div></div>
-                            </div>
-                            <div className={styles.price}>
-                                <p className={`${styles.digit} text text_type_digits-default`}>480</p>
-                                <CurrencyIcon type="primary" />
-                            </div>
-                        </div>
-                    </li>
-                    <li className={styles.order}>
-                        <div className={styles.orderid}>
-                            <p className={`${styles.id} text text_type_digits-default`}>#034535</p>
-                            <p className={`${styles.timestamp} text text_type_main-small text_color_inactive`}>Сегодня, 16:20 i-GMT+3</p>
-                        </div>
-                        <h3 className={`${styles.burgername} text text_type_main-default`}>Death Star Starship Main бургер</h3>
-                        <div className={styles.componentandprice}>
-                            <div className={styles.ingredients}>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview2} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview3} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview4} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview5} /></div></div>
-                            </div>
-                            <div className={styles.price}>
-                                <p className={`${styles.digit} text text_type_digits-default`}>480</p>
-                                <CurrencyIcon type="primary" />
-                            </div>
-                        </div>
-                    </li>
-                    <li className={styles.order}>
-                        <div className={styles.orderid}>
-                            <p className={`${styles.id} text text_type_digits-default`}>#034535</p>
-                            <p className={`${styles.timestamp} text text_type_main-small text_color_inactive`}>Сегодня, 16:20 i-GMT+3</p>
-                        </div>
-                        <h3 className={`${styles.burgername} text text_type_main-default`}>Death Star Starship Main бургер</h3>
-                        <div className={styles.componentandprice}>
-                            <div className={styles.ingredients}>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview2} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview3} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview4} /></div></div>
-                                <div className={styles.preview}><div className={styles.illustration}><img src={preview5} /></div></div>
-                            </div>
-                            <div className={styles.price}>
-                                <p className={`${styles.digit} text text_type_digits-default`}>480</p>
-                                <CurrencyIcon type="primary" />
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <div className={styles.completed}>
-                    <div className={styles.types}>
-                        <div className={styles.type}>
-                            <p className={`${styles.subtitle} text text_type_main-default`}>Готовы:</p>
-                            <p className={`${styles.digits} text text_type_digits-default`}>034533</p>
-                            <p className={`${styles.digits} text text_type_digits-default`}>034532</p>
-                            <p className={`${styles.digits} text text_type_digits-default`}>034530</p>
-                            <p className={`${styles.digits} text text_type_digits-default`}>034527</p>
-                            <p className={`${styles.digits} text text_type_digits-default`}>034525</p>
-                        </div>
-                        <div className={styles.type}>
-                        <p className={`${styles.subtitle} text text_type_main-default`}>В работе:</p>
-                            <p className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}>034533</p>
-                            <p className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}>034532</p>
-                            <p className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}>034530</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p className={`${styles.subtitle} text text_type_main-default`}>Выполнено за все время:</p>
-                        <p className={`${styles.digitslarge} text text_type_digits-large`}>28752</p>
-                    </div>
-                    <div>
-                        <p className={`${styles.subtitle} text text_type_main-default`}>Выполнено за сегодня:</p>
-                        <p className={`${styles.digitslarge} text text_type_digits-large`}>138</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+export function FeedPage() {
+  const dispatch = useDispatch();
+  const { orders } = useSelector((state) => state.ws);
+
+  const ingredientsData = useSelector(
+    (state) => state.data.burgerIngredients.fillings
+  );
+
+  const getCorrectOrders = (orders, data) => {
+    const correctOrders = [];
+    orders.forEach((order) => {
+      const { ingredients, ...rest } = order;
+      const correctIngredients = getIngredients(order.ingredients, data);
+      if (correctIngredients.length) {
+        correctOrders.push({ ...rest, ingredients: correctIngredients });
+      }
+    });
+    return correctOrders;
+  };
+  
+  const getDoneInProgressOrders = (orders) => {
+    const done = [];
+    const inProgress = [];
+    orders.forEach((order) => {
+      if (order.status === 'done') {
+        done.push(order.number);
+      } else {
+        inProgress.push(order.number);
+      }
+    });
+    return { done, inProgress };
+  };
+
+  const getIngredients = (ids, data) => {
+    const result = [];
+    const ingredients = new Map();
+    const buns = new Set();
+    ids.forEach((id) => {
+      const count = ingredients.get(id);
+      if (count) {
+        ingredients.set(id, count + 1);
+      } else {
+        ingredients.set(id, 1);
+      }
+    });
 }
 
+  const correctOrders = orders && getCorrectOrders(orders, ingredientsData);
+  const doneInProgressOrders =
+  correctOrders && getDoneInProgressOrders(correctOrders);
+
+  React.useEffect(() => {
+    dispatch(wsConnectionStartAction('wss://norma.nomoreparties.space/orders/all'));
+    return () => {
+      dispatch(wsConnectionClosedAction());
+    };
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    if (correctOrders && correctOrders.length && doneInProgressOrders) {
+      dispatch(setCorrectOrdersAction(correctOrders));
+      dispatch(setDoneOrdersAction(doneInProgressOrders));
+    }
+  });
+
+  if (!correctOrders) {
+    return (
+      <p
+        className={`${styles.text} text text_type_main-large text_color_inactive`}
+      >
+        Список пуст
+      </p>
+    );
+  }
+
+  return (
+    <>
+      <h2 className={`${styles.title} text text_type_main-large`}>
+        Лента заказов
+      </h2>
+      <div className={styles.content}>
+        <ul className={styles.orders}>
+            {orders.map(order => <OrderCard order={order} />)}
+        </ul>
+        <div className={styles.completed}>
+          <div className={styles.types}>
+            <div className={styles.type}>
+              <p className={`${styles.subtitle} text text_type_main-default`}>
+                Готовы:
+              </p>
+              <p className={`${styles.digits} text text_type_digits-default`}>
+                034533
+              </p>
+              <p className={`${styles.digits} text text_type_digits-default`}>
+                034532
+              </p>
+              <p className={`${styles.digits} text text_type_digits-default`}>
+                034530
+              </p>
+              <p className={`${styles.digits} text text_type_digits-default`}>
+                034527
+              </p>
+              <p className={`${styles.digits} text text_type_digits-default`}>
+                034525
+              </p>
+            </div>
+            <div className={styles.type}>
+              <p className={`${styles.subtitle} text text_type_main-default`}>
+                В работе:
+              </p>
+              <p
+                className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}
+              >
+                034533
+              </p>
+              <p
+                className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}
+              >
+                034532
+              </p>
+              <p
+                className={`${styles.digits} ${styles.digitsinactive} text text_type_digits-default`}
+              >
+                034530
+              </p>
+            </div>
+          </div>
+          <div>
+            <p className={`${styles.subtitle} text text_type_main-default`}>
+              Выполнено за все время:
+            </p>
+            <p className={`${styles.digitslarge} text text_type_digits-large`}>
+              28752
+            </p>
+          </div>
+          <div>
+            <p className={`${styles.subtitle} text text_type_main-default`}>
+              Выполнено за сегодня:
+            </p>
+            <p className={`${styles.digitslarge} text text_type_digits-large`}>
+              138
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
