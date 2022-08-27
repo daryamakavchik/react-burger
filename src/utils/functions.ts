43c306deab13d4396c5049fa9c3ecb647ffb61f4
daryamakavchik <darjamakavchik@gmail.com>
2022-08-26 21:28:07 -0700

    types for functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,6 @@
-export const filterOrders = (arr) => {
+export const filterOrders = (arr:Array<any>) => {
     return arr?.reduce(
-      (acc, curr) => {
+      (acc:any, curr:any) => {
         curr.status === "done"
           ? (acc.done = [...acc.done, curr])
           : (acc.pending = [...acc.pending, curr]);
@@ -10,13 +10,13 @@ export const filterOrders = (arr) => {
     );
   };
 
-export const editDate = (date) => {
+export const editDate = (date:any) => {
     const createdAt = new Date(date);
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     const diffTime = Math.ceil((today.getTime() - createdAt.getTime()) / (60 * 60 * 24 * 1000));
     const hours = createdAt.getHours() > 9 ? createdAt.getHours() : `0${createdAt.getHours()}`;
     const min = createdAt.getMinutes() > 9 ? createdAt.getMinutes() : `0${createdAt.getMinutes()}`;
-    const getDays = (days) => days === 0 ? "Сегодня" : days === 1 ? "Вчера" : days > 1 ? `${days} дня(-ей) назад` : "Ошибка";
+    const getDays = (days:any) => days === 0 ? "Сегодня" : days === 1 ? "Вчера" : days > 1 ? `${days} дня(-ей) назад` : "Ошибка";
     return `${getDays(diffTime)}, ${hours}:${min} i-GMT+${(createdAt.getTimezoneOffset() * -1) / 60}`;
   };
