92213adfd0ae78b332d0b848dc7369929960f7f2
daryamakavchik <darjamakavchik@gmail.com>
2022-09-20 20:59:33 -0700

    fix: hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,14 @@ import { Provider } from "react-redux";
 import { store } from "./services/store";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
-import { HashRouter} from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
     <DndProvider backend={HTML5Backend}>
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </DndProvider>
   </Provider>,
   document.getElementById("root")
