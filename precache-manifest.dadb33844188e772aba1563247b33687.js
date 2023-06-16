c7cb797e5820d15769345e6cfe6f0b5fc19992e7
daryamakavchik <darjamakavchik@gmail.com>
2023-06-15 21:02:12 -0700

    Updates

---
+++ precache-manifest.dadb33844188e772aba1563247b33687.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba24cc81f499095704ef54609a71cbe",
+    "url": "/react-burger/index.html"
+  },
+  {
+    "revision": "e79d7960a8a1d56a1243",
+    "url": "/react-burger/static/css/2.d6abe649.chunk.css"
+  },
+  {
+    "revision": "0690c996b49f30242594",
+    "url": "/react-burger/static/css/main.4edd46d0.chunk.css"
+  },
+  {
+    "revision": "e79d7960a8a1d56a1243",
+    "url": "/react-burger/static/js/2.b1ddadb0.chunk.js"
+  },
+  {
+    "revision": "f9e1841220d9a0d1283de90dddbdcbe1",
+    "url": "/react-burger/static/js/2.b1ddadb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0690c996b49f30242594",
+    "url": "/react-burger/static/js/main.53bae1f4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-burger/static/js/main.53bae1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d3cecc108dd2d2d92a",
+    "url": "/react-burger/static/js/runtime-main.96d816f8.js"
+  },
+  {
+    "revision": "190e82e91f8b44f23b28d50584aca336",
+    "url": "/react-burger/static/media/Iceland-Regular.190e82e9.woff2"
+  },
+  {
+    "revision": "b2cd58348e7bc3ea2f8035d824c80396",
+    "url": "/react-burger/static/media/Iceland-Regular.b2cd5834.woff"
+  },
+  {
+    "revision": "4ec5acf8515210125e80cf53d4a49ea3",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.4ec5acf8.woff2"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "974d6fa62992dad387572d8f3a13e8cc",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.974d6fa6.woff"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "13359a07227ea33ac84fc520f4dd172c",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.13359a07.woff2"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "2b64112a72baa46a570c02f6a4b4f2c7",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.2b64112a.woff"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "0c0abc2883d68596a6ae849a1b2d74ac",
+    "url": "/react-burger/static/media/graphics.0c0abc28.svg"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "/react-burger/static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "/react-burger/static/media/iceland.df042be2.woff"
+  }
+]);
